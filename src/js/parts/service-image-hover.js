12d35e379e1735a2a8c7b2dab2913ddb1b4c5102
MyCode07 <erem19992015@mail.ru>
2024-04-15 16:22:07 +0300

    home mobile

--- src/js/parts/service-image-hover.js
+++ src/js/parts/service-image-hover.js
@@ -30,3 +30,36 @@ if (categoriesWrapper) {
             })
         })
 }
+
+
+const scroll = document.querySelectorAll('.category-item__image div a');
+const links = document.querySelectorAll('.category-item ul li a');
+const defultImg = document.querySelector('.category-item__image ._default');
+let locked = false;
+
+if (links.length && window.innerWidth > 1024) {
+    const height = defultImg.getBoundingClientRect().height;
+
+    links.forEach((link, i) => {
+        link.addEventListener('mouseenter', () => {
+            locked = true
+            gsap.to(scroll, {
+                y: (i + 1) * -height,
+                duration: 0.7,
+            })
+        })
+
+        link.addEventListener('mouseleave', () => {
+            locked = false
+
+            setTimeout(() => {
+                if (locked == false) {
+                    gsap.to(scroll, {
+                        y: 0,
+                        duration: 1,
+                    })
+                }
+            }, 16);
+        })
+    })
+}
