fb6586926b36d7a93295139c4191754062f8174c
MyCode07 <erem19992015@mail.ru>
2024-06-07 13:26:57 +0300

    add some changes

--- src/js/parts/filters.js
+++ src/js/parts/filters.js
@@ -1,6 +1,9 @@
 import { lockPadding, unLockPadding } from '../utils/lockPadding.js';
 
 const filter = document.querySelector('.filter');
+const activeFilters = document.querySelector('.active-filters__body');
+const resetFilters = document.querySelector('.active-filters .reset-filters')
+
 
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
@@ -18,11 +21,38 @@ document.addEventListener('click', function (e) {
 
     if ((targetEl.closest('.select-body') || targetEl.closest('.select-input')) && targetEl.hasAttribute('data-id')) {
         const select = targetEl.closest('.select-input');
-        const label = select.querySelector('label')
+        const label = select.querySelector('span')
+        const input = targetEl.querySelector('input')
 
-        label.textContent = targetEl.textContent
-        label.dataset.id = targetEl.dataset.id
+
         select.classList.remove('_active')
+        resetFilters.classList.remove('_hide')
+
+
+        if (input) {
+            const item = `<button class="active-filters__item" data-custom-field="${input.id}">${targetEl.textContent}</button>`
+            if (!activeFilters.querySelector(`[data-custom-field="${input.id}"]`)) {
+                activeFilters.insertAdjacentHTML('afterbegin', item)
+            }
+        }
+
+    }
+
+    if (targetEl.classList.contains('active-filters__item')) {
+        targetEl.remove()
+
+        if (!document.querySelector('.active-filters__item')) {
+            resetFilters.classList.add('_hide')
+        }
+    }
+
+    if (targetEl.hasAttribute('data-reset-filters')) {
+        const activeFilters = document.querySelectorAll('.active-filters__item');
+        if (activeFilters.length) {
+            activeFilters.forEach(item => item.remove())
+        }
+
+        resetFilters.classList.add('_hide')
     }
 
     if (!targetEl.classList.contains('select-input') && !targetEl.closest('.select-input') && document.querySelector('.select-input._active')) {
@@ -34,9 +64,11 @@ document.addEventListener('click', function (e) {
         filter.classList.add('_open')
         lockPadding();
     }
-    
+
     if (targetEl.hasAttribute('data-close-filter')) {
         filter.classList.remove('_open')
         unLockPadding();
     }
 })
+
+
--- src/js/parts/range.js
+++ src/js/parts/range.js
@@ -1,6 +1,8 @@
 import dhx from 'dhx-suite'
 
 const formatter = new Intl.NumberFormat("ru");
+const activeFilters = document.querySelector('.active-filters__body');
+const resetFilters = document.querySelector('.active-filters .reset-filters')
 
 
 export function range() {
@@ -10,8 +12,8 @@ export function range() {
 
     ranges.forEach(range => {
 
-        const minOutElem = range.querySelector('.range-min ins');
-        const maxOutElem = range.querySelector('.range-max ins');
+        const minOutElem = range.querySelector('.range-min');
+        const maxOutElem = range.querySelector('.range-max');
         const min = +range.dataset.min ? +range.dataset.min : 0;
         const max = +range.dataset.max ? +range.dataset.max : 0;
         const step = +range.dataset.step ? +range.dataset.step : 1;
@@ -27,11 +29,68 @@ export function range() {
 
         sliderRange.events.on('change', () => {
             minOutElem.dataset.value = sliderRange.getValue()[0]
-            minOutElem.textContent = formatter.format(sliderRange.getValue()[0])
+            minOutElem.querySelector('ins').textContent = formatter.format(sliderRange.getValue()[0])
 
             maxOutElem.dataset.value = sliderRange.getValue()[1]
-            maxOutElem.textContent = formatter.format(sliderRange.getValue()[1])
-        });
-    });
+            maxOutElem.querySelector('ins').textContent = formatter.format(sliderRange.getValue()[1])
 
+            if (sliderRange.getValue()[0] > min) {
+                createFitlerItem('price_min', 'min ' + minOutElem.textContent)
+            }
+            else {
+                removeFitlerItem('price_min')
+            }
+
+            if (sliderRange.getValue()[1] < max) {
+                createFitlerItem('price_max', 'max ' + maxOutElem.textContent)
+            }
+            else {
+                removeFitlerItem('price_max')
+            }
+
+        });
+
+        document.addEventListener('click', function (e) {
+            let targetEl = e.target;
+
+            if (targetEl.dataset.customField == 'price_min') {
+                sliderRange.setValue([min, sliderRange.getValue()[1]])
+            }
+            if (targetEl.dataset.customField == 'price_max') {
+                sliderRange.setValue([sliderRange.getValue()[0], max])
+            }
+
+            if (targetEl.classList.contains('reset-filters') && sliderRange) {
+                sliderRange.setValue([min, max])
+            }
+        })
+
+    });
+}
+
+
+
+function createFitlerItem(id, text) {
+    const item = `<button class="active-filters__item" data-custom-field="${id}">${text}</button>`
+
+    const activeItem = activeFilters.querySelector(`[data-custom-field="${id}"]`)
+    if (!activeItem) {
+        activeFilters.insertAdjacentHTML('afterbegin', item)
+    }
+    else {
+        activeItem.textContent = text
+    }
+
+    resetFilters.classList.remove('_hide')
+}
+
+function removeFitlerItem(id) {
+    const activeItem = activeFilters.querySelector(`[data-custom-field="${id}"]`)
+    if (activeItem) {
+        activeItem.remove();
+    }
+
+    if (!document.querySelector('.active-filters__item')) {
+        resetFilters.classList.add('_hide')
+    }
 }
