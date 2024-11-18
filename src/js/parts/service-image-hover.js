1820668dc601bc11e0e1864d4f130fc0e4d8cf01
MyCode07 <erem19992015@mail.ru>
2024-11-18 12:43:45 +0300

    add changes into wp

--- src/js/parts/service-image-hover.js
+++ src/js/parts/service-image-hover.js
@@ -1,36 +1,33 @@
 import { gsap } from 'gsap'
 import { isMobile } from '../utils/isMobile.js'
 
-const categoriesWrapper = document.querySelector('.about-services .section__bottom')
-
-if (categoriesWrapper) {
-    categoriesWrapper.addEventListener('mousemove', e => {
-        gsap.to('img', {
-            x: e.x,
-            y: e.y,
-            // transform origin of images to center
-            xPercent: -50,
-            yPercent: -125,
-            stagger: .02
-        })
-    })
-
+const aboutServices = document.querySelector('.about-services .section__bottom a')
+if (aboutServices) {
     gsap.utils.toArray('.about-services .section__bottom a')
-        .forEach(category => {
-            let { label } = category.dataset
+        .forEach(item => {
+            let { label } = item.dataset
+            const image = item.querySelector(`img[data-image=${label}]`);
+            const imageSizes = image.getBoundingClientRect();
 
-            category.addEventListener('mouseenter', () => {
-                gsap.to(`img[data-image=${label}]`, { opacity: 1, scale: 1 })
-                gsap.set(`img[data-image=${label}]`, { zIndex: 1 })
-                gsap.set(`a[data-label=${label}]`, { zIndex: 2 })
+            item.addEventListener('mousemove', (e) => {
+                gsap.to(image, {
+                    x: e.clientX / 2 + imageSizes.width,
+                    y: -e.clientY / 2 - imageSizes.heigth,
+                    left: 0,
+                    top: 0,
+                })
+                gsap.to(image, { opacity: 1, scale: 1 })
+                gsap.set(image, { zIndex: 1 })
+                gsap.set(item, { zIndex: 2 })
             })
 
-            category.addEventListener('mouseleave', () => {
-                gsap.to(`img[data-image=${label}]`, { opacity: 0, zIndex: -1, scale: .80 })
-                gsap.set(`a[data-label=${label}]`, { zIndex: 0 })
+            item.addEventListener('mouseleave', (e) => {
+                gsap.to(image, { opacity: 0, zIndex: -1, scale: .80 })
+                gsap.set(item, { zIndex: 0 })
             })
         })
 }
+
 
 
 const categorySections = document.querySelectorAll('.category .category-item');
